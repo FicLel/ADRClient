ab5b77ab499cbf6985aa962c19642f521759d476
FicLel <victorleal190@gmail.com>
2021-01-13 02:25:20 +0100

    tables

---
+++ src/views/Level.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-container>
+      <tables/>
+    </v-container>
+  </div>
+</template>
+<script>
+import tables from '@/components/Table/show-tables.vue';
+export default {
+  components: {
+    tables,
+  },
+}; 
+</script>
